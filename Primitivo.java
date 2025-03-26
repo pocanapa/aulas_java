75ae7631de271241dc6a83b6a6fe6ec27d1d9d1e
pietro <p13tr0gaud1ano@gmail.com>
2025-03-25 23:42:40 -0300

    Adicionando Primitivo // Constante // Entrada de dado // Entrada com janela

---
+++ Primitivo.java
@@ -0,0 +1,32 @@
+package br.com.fiap;
+
+public class Primitivo {
+    public static void main(String[] args){
+        // declaração e inicialização de variáveis
+        char sexo = 'F';
+        byte idade =35;
+        short codigo = 15650;
+        int alunos = 50, classes = 11;
+        long brasileiros = 216445377500L;
+        float media = 8.7f;
+        double dolar = 5.68;
+        boolean alternativa = false;
+        // Não Primitivo
+        String nomeCompleto = "Astrogildo Celeste";
+        System.out.println("Sexo: " + sexo + " Idade: " + idade + " Código: " + codigo);
+        System.out.println("Média: " + media + " Alunos: " + alunos + " Trumas: " + classes);
+        System.out.println("Habitantes: " + brasileiros + " Cotação: " + dolar);
+        System.out.println("Nome: " + nomeCompleto);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
